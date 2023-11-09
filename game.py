b0e84a910ac0df9bc1a543ee65f7a33050b68552
R9_ <neamtrares@naver.com>
2023-11-09 17:34:35 +0200

    Player

--- game.py
+++ game.py
@@ -14,9 +14,20 @@ def main():
   text = Text2D("Test", position=Vector2(550, 0))
   floor = Rect2D(1, Vector2(0, 660), (255, 0, 0), Vector2(1280, 720))
   text.init(window)
+  rect = Rect2D(1, position=Vector2(100, 150), size=Vector2(50, 80))
   floor.init(window)
+  rect.init(window)
   while window.running:
       keys = window.frame()
+      vel = Vector2(0, 0)
+      if keys[key_to_scancode(" ")] and rect.is_on_floor():
+         vel.y = -35
+      vel.x = (keys[key_to_scancode("d")]-keys[key_to_scancode("a")])*2
+      print(vel)
+      rect.move(vel)
+      if not rect.is_on_floor():
+         rect.move(vec=Vector2(0, 1))
+         #rect.move(vec=Vector2(0, 1))
       if keys[27]:
           return 1
 
