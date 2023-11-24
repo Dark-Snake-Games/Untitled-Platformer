b17a993bdf6d61b4d3fcfd7c202abfd69cc46286
R9_ <neamtrares@naver.com>
2023-11-24 20:07:57 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 from DSEngine import *
 class Player(AnimatedSprite2D):
     def __init__(self, position: Vector2):
-        sheet = AnimationSheet(default=Image2D("Test.png"))
+        sheet = AnimationSheet(default=Image2D("assets/Test.png"))
         self.jumps = 0
-        super().__init__(sheet, 1, position)
+        super().__init__(sheet, 2, position)
     
     def render(self, window: Window):
         vel = Vector2(0, 0)
@@ -25,12 +25,14 @@ class Player(AnimatedSprite2D):
 def main():
   window = Window(title="Test", fps=120, size=(1280, 720), bg=(100, 100, 100))
   audio_man = AudioManager()
+  bg = Image2D("assets/bg_resize.png")
   text = Text2D("Test", position=Vector2(550, 0))
-  floor = Rect2D(1, Vector2(0, 660), (255, 0, 0), Vector2(1280, 720))
+  floor = Rect2D(2, Vector2(0, 660), (255, 0, 0), Vector2(1280, 720))
   player = Player(position=Vector2(100, 150))
   player.init(window)
   text.init(window)
   floor.init(window)
+  bg.init(window)
   while window.running:
       keys = window.frame()
       if keys[27]:
